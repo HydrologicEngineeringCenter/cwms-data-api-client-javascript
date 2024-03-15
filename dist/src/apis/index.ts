99bd68188d913c0d919952350177f8826163d6da
krowvin <charlie@krowvin.com>
2024-03-15 17:11:25 -0500

    Cleaned up root dir. Added openapi.config.json file with "moduleName" (broken before).

---
+++ dist/src/apis/index.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthorizationApi';
+export * from './BasinsApi';
+export * from './BlobApi';
+export * from './CatalogApi';
+export * from './ClobApi';
+export * from './CountiesApi';
+export * from './DefaultApi';
+export * from './LevelsApi';
+export * from './LocationCategoriesApi';
+export * from './LocationGroupsApi';
+export * from './LocationsApi';
+export * from './OfficesApi';
+export * from './ParametersApi';
+export * from './PoolsApi';
+export * from './RatingsApi';
+export * from './StatesApi';
+export * from './TimeSeriesApi';
+export * from './TimeSeriesCategoriesApi';
+export * from './TimeSeriesIdentifierApi';
+export * from './TimeZonesApi';
+export * from './TimeseriesGroupsApi';
+export * from './UnitsApi';
