3bd240d69a7907b9adad455bdd0ed8614d3c216a
Charles Graham <charles.r.graham@usace.army.mil>
2024-04-19 16:47:22 -0500

    Add dist - include bundle

---
+++ src/dist/apis/index.js
@@ -0,0 +1,40 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/* tslint:disable */
+/* eslint-disable */
+__exportStar(require("./AuthorizationApi"), exports);
+__exportStar(require("./BasinsApi"), exports);
+__exportStar(require("./BlobApi"), exports);
+__exportStar(require("./CatalogApi"), exports);
+__exportStar(require("./ClobApi"), exports);
+__exportStar(require("./CountiesApi"), exports);
+__exportStar(require("./DefaultApi"), exports);
+__exportStar(require("./LevelsApi"), exports);
+__exportStar(require("./LocationCategoriesApi"), exports);
+__exportStar(require("./LocationGroupsApi"), exports);
+__exportStar(require("./LocationsApi"), exports);
+__exportStar(require("./OfficesApi"), exports);
+__exportStar(require("./ParametersApi"), exports);
+__exportStar(require("./PoolsApi"), exports);
+__exportStar(require("./RatingsApi"), exports);
+__exportStar(require("./StatesApi"), exports);
+__exportStar(require("./TimeSeriesApi"), exports);
+__exportStar(require("./TimeSeriesCategoriesApi"), exports);
+__exportStar(require("./TimeSeriesIdentifierApi"), exports);
+__exportStar(require("./TimeZonesApi"), exports);
+__exportStar(require("./TimeseriesGroupsApi"), exports);
+__exportStar(require("./UnitsApi"), exports);
---
+++ src/dist/models/index.js
@@ -0,0 +1,79 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/* tslint:disable */
+/* eslint-disable */
+__exportStar(require("./ApiKey"), exports);
+__exportStar(require("./AssignedLocation"), exports);
+__exportStar(require("./AssignedTimeSeries"), exports);
+__exportStar(require("./Basin"), exports);
+__exportStar(require("./Blob"), exports);
+__exportStar(require("./Blobs"), exports);
+__exportStar(require("./Catalog"), exports);
+__exportStar(require("./CatalogEntry"), exports);
+__exportStar(require("./CatalogableEndpoint"), exports);
+__exportStar(require("./CdaError"), exports);
+__exportStar(require("./Clob"), exports);
+__exportStar(require("./Clobs"), exports);
+__exportStar(require("./County"), exports);
+__exportStar(require("./CsvV1Office"), exports);
+__exportStar(require("./DatabaseLoadMethod"), exports);
+__exportStar(require("./DeleteMethod"), exports);
+__exportStar(require("./IndependentRoundingSpec"), exports);
+__exportStar(require("./Location"), exports);
+__exportStar(require("./LocationAlias"), exports);
+__exportStar(require("./LocationCatalogEntry"), exports);
+__exportStar(require("./LocationCategory"), exports);
+__exportStar(require("./LocationGroup"), exports);
+__exportStar(require("./LocationLevel"), exports);
+__exportStar(require("./LocationLevels"), exports);
+__exportStar(require("./Office"), exports);
+__exportStar(require("./OfficeCSV"), exports);
+__exportStar(require("./OfficeFormatV1"), exports);
+__exportStar(require("./OfficeTabulation"), exports);
+__exportStar(require("./OfficesFMT"), exports);
+__exportStar(require("./Offset"), exports);
+__exportStar(require("./ParameterSpec"), exports);
+__exportStar(require("./Pool"), exports);
+__exportStar(require("./PoolNameType"), exports);
+__exportStar(require("./Pools"), exports);
+__exportStar(require("./RatingSpec"), exports);
+__exportStar(require("./RatingSpecs"), exports);
+__exportStar(require("./RatingTemplate"), exports);
+__exportStar(require("./RatingTemplates"), exports);
+__exportStar(require("./SeasonalValueBean"), exports);
+__exportStar(require("./SpecifiedLevel"), exports);
+__exportStar(require("./State"), exports);
+__exportStar(require("./StoreRule"), exports);
+__exportStar(require("./Stream"), exports);
+__exportStar(require("./StreamLocation"), exports);
+__exportStar(require("./StreamReach"), exports);
+__exportStar(require("./TabV1Office"), exports);
+__exportStar(require("./TimeSeries"), exports);
+__exportStar(require("./TimeSeriesCategory"), exports);
+__exportStar(require("./TimeSeriesColumn"), exports);
+__exportStar(require("./TimeSeriesExtents"), exports);
+__exportStar(require("./TimeSeriesGroup"), exports);
+__exportStar(require("./TimeSeriesIdentifierDescriptor"), exports);
+__exportStar(require("./TimeSeriesIdentifierDescriptors"), exports);
+__exportStar(require("./TimeSeriesInterval"), exports);
+__exportStar(require("./TimeSeriesIntervalUnitsInner"), exports);
+__exportStar(require("./TimeSeriesIntervalUnitsInnerDuration"), exports);
+__exportStar(require("./TimeSeriesRecord"), exports);
+__exportStar(require("./TimeSeriesValuesInnerInner"), exports);
+__exportStar(require("./TimeseriesCatalogEntry"), exports);
+__exportStar(require("./UnitSystem"), exports);
+__exportStar(require("./VerticalDatumInfo"), exports);
