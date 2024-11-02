fc1a0171b054d0e3d0e8f38d5cf07d98e3e44a2a
Charles Graham, SWT <charles.r.graham@usace.army.mil>
2024-11-01 21:41:08 -0500

    Update tests

--- tests/endpoints/Timeseries.v2.test.js
+++ tests/endpoints/Timeseries.v2.test.js
@@ -1,23 +1,22 @@
 // Example / test for fetching timeseries from CDA
 
 import { TimeSeriesApi, Configuration } from "cwmsjs";
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 global.fetch = fetch;
 
-test('Test Timeseries V2', async () => {
-
-    const v2_config = new Configuration({
-        "headers": {
-            "accept": "application/json;version=2",
-        }
+test("Test Timeseries V2", async () => {
+  const v2_config = new Configuration({
+    headers: {
+      accept: "application/json;version=2",
+    },
+  });
+  const ts_api = new TimeSeriesApi(v2_config);
+  await ts_api
+    .getCwmsDataTimeseries({
+      office: "SWT",
+      name: "KEYS.Elev.Inst.1Hour.0.Ccp-Rev",
     })
-    const ts_api = new TimeSeriesApi(v2_config);
-    await ts_api.getCwmsDataTimeseries({
-        "office": "SWT",
-        "name": "KEYS.Elev.Inst.1Hour.0.Ccp-Rev",
-    })
-        .then((data) => {
-            expect(data?.values).toBeDefined()
-        })
-}, 30000)
-
+    .then((data) => {
+      expect(data?.values).toBeDefined();
+    });
+}, 30000);
