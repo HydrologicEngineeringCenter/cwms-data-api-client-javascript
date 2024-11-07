697553907f874090d81cb36880e48a602fe4c186
Brandon Kolze <james.b.kolze@usace.army.mil>
2024-11-07 15:06:11 -0500

    Add BaseRatingMetadata to fix type issues
    Base CDA OpenAPI spec appears to have some typing issues with
    AbstractRatingMetadata.  Add and use BaseRatingMetadata to fix build
    process.

--- swagger-updates/modSpec.sh
+++ swagger-updates/modSpec.sh
@@ -3,5 +3,9 @@
 # Add default servers to spec
 npx node-jq '.servers = inputs.servers' cwms-swagger-raw.json swagger-updates/servers.json |
 
+# Add BaseRatingMetadata to fix typing issues
+npx node-jq --slurpfile base swagger-updates/BaseRatingMetadata.json '.components.schemas.BaseRatingMetadata = $base[0].BaseRatingMetadata' |
+sed 's/"\$ref": "#\/components\/schemas\/AbstractRatingMetadata"/"\$ref": "#\/components\/schemas\/BaseRatingMetadata"/g' |
+
 # Write to file
 cat > cwms-swagger-mod.json
