f27bbc1851829d17cab5aa410324eb82b1340733
Brandon Kolze <james.b.kolze@usace.army.mil>
2024-11-08 10:28:17 -0500

    Automate fixing of TimeSeries values typing bug
    In modified OpenAPI spec, change the TimeSeries "values" key from an
    array of arrays of objects to an array of arrays of numbers.

--- scripts/spec-updates/modSpec.sh
+++ scripts/spec-updates/modSpec.sh
@@ -7,5 +7,8 @@ npx node-jq '.servers = inputs.servers' cwms-swagger-raw.json scripts/spec-updat
 npx node-jq --slurpfile base scripts/spec-updates/BaseRatingMetadata.json '.components.schemas.BaseRatingMetadata = $base[0].BaseRatingMetadata' |
 sed 's/"\$ref": "#\/components\/schemas\/AbstractRatingMetadata"/"\$ref": "#\/components\/schemas\/BaseRatingMetadata"/g' |
 
+# Fix typing of TimeSeries response values
+npx node-jq --slurpfile tsItems scripts/spec-updates/tsArrayItems.json '.components.schemas.TimeSeries.properties.values.items.items = $tsItems[0].items' |
+
 # Write to file
 cat > cwms-swagger-mod.json
