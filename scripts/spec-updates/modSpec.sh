6ae9565d1de978918a3b1d4a3cb2068c12c544f5
Brandon Kolze <james.b.kolze@usace.army.mil>
2024-11-08 08:19:32 -0500

    Move swagger-updates to scripts/spec-updates

--- scripts/spec-updates/modSpec.sh
+++ scripts/spec-updates/modSpec.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Add default servers to spec
-npx node-jq '.servers = inputs.servers' cwms-swagger-raw.json swagger-updates/servers.json |
+npx node-jq '.servers = inputs.servers' cwms-swagger-raw.json scripts/spec-updates/servers.json |
 
 # Add BaseRatingMetadata to fix typing issues
-npx node-jq --slurpfile base swagger-updates/BaseRatingMetadata.json '.components.schemas.BaseRatingMetadata = $base[0].BaseRatingMetadata' |
+npx node-jq --slurpfile base scripts/spec-updates/BaseRatingMetadata.json '.components.schemas.BaseRatingMetadata = $base[0].BaseRatingMetadata' |
 sed 's/"\$ref": "#\/components\/schemas\/AbstractRatingMetadata"/"\$ref": "#\/components\/schemas\/BaseRatingMetadata"/g' |
 
 # Write to file
